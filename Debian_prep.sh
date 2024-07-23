75f117499a7b01088ba6eacc5218dcd4258b42b9
Milen Popov <milen.popov@outlook.com>
2024-07-23 10:02:19 +0300

    g

--- Debian_prep.sh
+++ Debian_prep.sh
@@ -16,12 +16,22 @@ print_ok() {
 
 # Update Debian 12 to the latest
 print_header "Updating Debian 12 to the latest..."
-sudo apt update && sudo apt upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
+print_ok
+
+# Perform a dist-upgrade to handle dependencies
+print_header "Performing dist-upgrade..."
+sudo apt-get dist-upgrade -y
+print_ok
+
+# Install apt-transport-https
+print_header "Installing apt-transport-https..."
+sudo apt-get install apt-transport-https -y
 print_ok
 
 # Install Python 3, pip, virtualenv, and other necessary packages
 print_header "Installing Python 3, pip, virtualenv, fortune, lolcat, and cowsay..."
-sudo apt install python3 python3-pip python3-venv fortune lolcat cowsay -y
+sudo apt-get install python3 python3-pip python3-venv fortune lolcat cowsay -y
 print_ok
 
 # Create a virtual environment
@@ -40,13 +50,51 @@ pip install psutil
 pip install termcolor
 print_ok
 
+# Create a folder for Elasticsearch certificates
+print_header "Creating a folder for Elasticsearch certificates..."
+sudo mkdir -p /etc/elasticsearch/certificates
+print_ok
+
+# Add Elasticsearch GPG key directly to the new keyring
+print_header "Adding Elasticsearch GPG key..."
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+print_ok
+
+# Save the repository definition
+print_header "Saving Elasticsearch repository definition..."
+echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+print_ok
+
+# Update package lists
+print_header "Updating package lists..."
+sudo apt-get update
+print_ok
+
+# Install Elasticsearch
+print_header "Installing Elasticsearch..."
+sudo apt-get install elasticsearch -y
+print_ok
+
+# Enable and start Elasticsearch service
+print_header "Enabling and starting Elasticsearch service..."
+sudo systemctl enable elasticsearch
+sudo systemctl start elasticsearch
+print_ok
+
 # Confirm installations
 print_header "Confirming installations..."
 python --version
+print_ok
+echo -e "\n\n"
 pip --version
+print_ok
+echo -e "\n\n"
 pip show psutil
+print_ok
+echo -e "\n\n"
 pip show termcolor
 print_ok
+echo -e "\n\n"
 
 echo "Setup complete. To use the virtual environment, run 'source /opt/elasticsearch_env/bin/activate'."
 
